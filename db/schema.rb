d96c10ec4636ee24ac79fb69460f02f55227ee7b
sabricakir <sabricakir86@gmail.com>
2023-06-02 15:14:46 +0300

    Create game scaffold and add 10 random data to index via seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_163342) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_120903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.string "title"
+    t.text "description"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do |index|
+  starts_at = index.days.ago
+  ends_at = starts_at + 2*60*60
+  title = Faker::Sport.sport
+  description = Faker::Quote.famous_last_words
+  address = Faker::Address.full_address
+  game = Game.create(starts_at:, ends_at:, title:, description:, address:)
+end
