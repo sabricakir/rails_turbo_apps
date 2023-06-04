002d0bf2e1ef7234db4eac197085ebf469a15f51
sabricakir <sabricakir86@gmail.com>
2023-06-04 23:30:52 +0300

    Add turbo frames for inline editing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,7 @@ class GamesController < ApplicationController
 
   # GET /games or /games.json
   def index
-    @games = Game.all
+    @games = Game.all.order(created_at: :desc)
   end
 
   # GET /games/1 or /games/1.json
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,5 @@
 class Game < ApplicationRecord
+  EDITABLE_ATTRIBUTES = %i[title description starts_at ends_at].freeze
+
+  validates :title, :description, :starts_at, :ends_at, presence: true
 end
