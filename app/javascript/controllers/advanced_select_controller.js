3c01678a357444182eacdb2c3425340bc74046af
sabricakir <sabricakir86@gmail.com>
2023-06-04 22:48:14 +0300

    Add close on select settings for selectbox

--- app/javascript/controllers/advanced_select_controller.js
+++ app/javascript/controllers/advanced_select_controller.js
@@ -6,7 +6,9 @@ export default class extends Controller {
   connect() {
     new SlimSelect({
       select: this.element,
-      closeOnSelect: false
+      settings: {
+        closeOnSelect: false
+      }
     })
   }
 }
