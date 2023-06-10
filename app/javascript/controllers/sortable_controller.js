1e796187c020df9efd7a310b91823a071721ee4e
sabricakir <sabricakir86@gmail.com>
2023-06-10 20:39:04 +0300

    Get list id that we moved

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -4,6 +4,12 @@ import Sortable from 'sortablejs';
 // Connects to data-controller="sortable"
 export default class extends Controller {
   connect() {
-    Sortable.create(this.element, {})
+    Sortable.create(this.element, {
+        onEnd: this.onEnd.bind(this)
+    })
+  }
+  onEnd(event) {
+    //console.log(event.newIndex)
+    console.log(event.item.dataset.sortableId)
   }
 }
