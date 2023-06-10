e8a784a6288b7dd5def9854360c6b4bf45a32766
sabricakir <sabricakir86@gmail.com>
2023-06-10 19:35:37 +0300

    Add recently viewed products to user and display it

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find_by!(id: params[:id])
+    Current.user&.recently_viewed_products << @product.id
   end
 end
