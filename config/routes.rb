79f1b3a757e4d5fdebc27f59e93e245b1b02a37c
sabricakir <sabricakir86@gmail.com>
2023-06-10 19:18:51 +0300

    Add product model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     end
   end
   resources :posts
+  resources :products, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
