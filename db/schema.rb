5d0fdd8887e382fe6a18bafd1ae75a6402aef5b9
sabricakir <sabricakir86@gmail.com>
2023-06-18 15:56:07 +0300

    Create room scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_180729) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_125220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_180729) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
