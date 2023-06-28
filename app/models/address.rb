ae138215ce4b164a8ecaa4384561bd2aed84d29d
sabricakir <sabricakir86@gmail.com>
2023-06-28 17:19:44 +0300

    Add validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,19 @@
 class Address < ApplicationRecord
+  validates :country, presence: true
+  validates :state, inclusion: { in: -> (address) { address.states.keys }, allow_blank: true }
+  validates :state, presence: { if: -> (address) { address.states.present? }}
+  validates :city, inclusion: { in: -> (address) { address.cities }, allow_blank: true }
+  validates :city, presence: { if: -> (address) { address.cities.present? }}
+  validates :details, presence: true
+  def countries
+    CS.countries
+  end
+
+  def states
+    CS.states(country).with_indifferent_access
+  end
+
+  def cities
+    CS.cities(state, country) || []
+  end
 end
