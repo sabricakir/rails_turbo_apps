36e37fd6212a79933bf915c4add3a71673730901
sabricakir <sabricakir86@gmail.com>
2023-06-28 15:36:20 +0300

    Complete adding autocomplete search

---
+++ app/javascript/controllers/debounce_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="debounce"
+export default class extends Controller {
+  static targets = [ "form" ]
+  connect() {
+    console.log('Hello from debounce controller')
+  }
+
+  search(event) {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+      this.formTarget.requestSubmit()
+    }, 500)
+  }
+}
