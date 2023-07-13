9a3a7e5a7c6f3161fb509c9a82f25bc5f1addfc8
sabricakir <sabricakir86@gmail.com>
2023-07-13 14:48:51 +0300

    Add active_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+  def active_link_to(text=nil, path=nil, html_options={}, &block)
+    link = block_given? ? text : path
+    html_options[:class] = class_names(html_options[:class], 'text-green-500 font-bold') if current_page?(link)
+    if block_given?
+      link_to(text, html_options, &block)
+    else
+      link_to(text, path, html_options)
+    end
+  end
 end
