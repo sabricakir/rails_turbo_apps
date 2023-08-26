3a6d15bd58dd58c453376532bad1b9ad9118faa4
sabricakir <sabricakir86@gmail.com>
2023-08-26 16:53:14 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,7 +59,22 @@ end
   # Movie.create title: Faker::Movie.title
 end
 
-10.times do |_index|
-  Location.create(name: Faker::Address.city, address: Faker::Address.full_address, latitude: Faker::Address.latitude,
-                  longitude: Faker::Address.longitude)
-end
+name = 'Hôtel Martinez - The Unbound Collection by Hyatt'
+address = '73 Bd de la Croisette, 06400 Cannes'
+Location.create(name:, address:)
+
+name = 'Exclusive Hotel Belle Plage'
+address = '2 Rue Brougham, 06400 Cannes'
+Location.create(name:, address:)
+
+name = 'Best Western Premier Le Patio des Artistes - Cannes'
+address = '6 Rue de Bône, 06400 Cannes'
+Location.create(name:, address:)
+
+name = 'Le Negresco'
+address = '37 Prom. des Anglais, 06000 Nice'
+Location.create(name:, address:)
+
+name = 'Caesars Palace'
+address = '3570 S Las Vegas Blvd, Las Vegas, NV 89109, United States'
+Location.create(name:, address:)
